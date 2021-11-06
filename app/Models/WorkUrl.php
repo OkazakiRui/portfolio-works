448f36c9c8161a957e4233ce075cd40b4a5027f6
RuiOkazaki <okazaki200226@gmail.com>
2021-11-06 20:37:53 +0900

    add: created WorkUrl model

---
+++ app/Models/WorkUrl.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WorkUrl extends Model
+{
+    protected $table = "works_url";
+    use HasFactory;
+}
